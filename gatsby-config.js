37e2a07867985a983a149b253aafda907142a75b
Mitchell Christ <mitchell@human.marketing>
2021-01-13 15:29:53 -0800

    Revert "Revert "add csp plugin and directives for optimize""
    This reverts commit 0687744c6ffc064c920f24dc40606b7b3a3a158f.

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,26 +60,42 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-csp',
+      options: {
+        directives: {
+          'script-src': "https://www.google-analytics.com 'unsafe-inline';",
+          'style-src':  "'unsafe-inline'; ",
+          'img-src':    "https://www.google-analytics.com",
+
+          'script-src': "https://optimize.google.com 'unsafe-inline';",
+          'style-src':  "https://optimize.google.com https://fonts.googleapis.com 'unsafe-inline';",
+          'img-src':    "https://optimize.google.com;",
+          'font-src':   "https://fonts.gstatic.com;",
+          'frame-src':  "https://optimize.google.com",
+        }
+      }
+    },
+    {
       resolve: "gatsby-plugin-google-tagmanager",
       options: {
         id: "GTM-TMBJCT7",
-  
+
         // Include GTM in development.
         //
         // Defaults to false meaning GTM will only be loaded in production.
         includeInDevelopment: false,
-  
+
         // datalayer to be set before GTM is loaded
         // should be an object or a function that is executed in the browser
         //
         // Defaults to null
         defaultDataLayer: { platform: "gatsby" },
-  
+
         // Specify optional GTM environment details.
         //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
         //gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
         //dataLayerName: "YOUR_DATA_LAYER_NAME",
-  
+
         // Name of the event that is triggered
         // on every Gatsby route change.
         //
